83d8307a194206cd03c4ef30ee34755b081a4dd2
Lance Lin <lancel@LANCEL-MACAIR.local>
2016-03-12 22:31:28 -0800

    fix problems and adding asynch file

--- ui.R
+++ ui.R
@@ -1,16 +1,9 @@
-
-# This is the user-interface definition of a Shiny web application.
-# You can find out more about building applications with Shiny here:
-# 
-# http://www.rstudio.com/shiny/
-#
-
 library(shiny)
 
-shinyUI(pageWithSidebar(
+shinyUI(fluidPage(
   
   # Application title
-  headerPanel("Old Faithful Geyser Data"),
+  titlePanel("LendingClub Loan Listing Data"),
   
   # Sidebar with a slider input for number of bins
   sidebarPanel(
@@ -24,6 +17,7 @@ shinyUI(pageWithSidebar(
   
   # Show a plot of the generated distribution
   mainPanel(
-    plotOutput("distPlot")
+    #plotOutput("distPlot"),
+    tableOutput("dailyLoan")
   )
 ))
